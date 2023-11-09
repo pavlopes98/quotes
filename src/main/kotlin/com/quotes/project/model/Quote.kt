081723344b089940a84e8300dd14bdff5b359d52
pavlopes98 <pavlopes1998@gmail.com>
2023-11-09 14:52:30 +0000

    Add Index to Quote model
    This commit adds a new index to the quoteAuthor attribute for the Quote data
    model. This way the read operations performance are increased.

--- src/main/kotlin/com/quotes/project/model/Quote.kt
+++ src/main/kotlin/com/quotes/project/model/Quote.kt
@@ -2,12 +2,14 @@ package com.quotes.project.model
 
 import org.springframework.data.annotation.Id
 import org.springframework.data.mongodb.core.mapping.Document
+import org.springframework.data.mongodb.core.index.Indexed
 
 @Document(collection = "quotes")
 data class Quote (
     @Id
     val _id: String,
     val quoteText: String,
+    @Indexed
     val quoteAuthor: String,
     val quoteGenre: String,
     val __v: Int
